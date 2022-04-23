1377757672d8fa5a4377def264cc58fb06cc55a4
Inanc <inancakduvann@gmail.com>
2022-04-24 00:11:31 +0300

    init

---
+++ main.js
@@ -0,0 +1,42 @@
+const connectButton = document.getElementById("connectButton");
+const reloadButton = document.getElementById("reloadButton");
+const installAlert = document.getElementById("installAlert");
+const walletID = document.getElementById("walletID");
+
+const startLoading = () => {
+  connectButton.classList.add("loadingButton");
+};
+
+const stopLoading = () => {
+  const timeout = setTimeout(() => {
+    connectButton.classList.remove("loadingButton");
+    clearTimeout(timeout);
+  }, 300);
+};
+
+connectButton.addEventListener("click", () => {
+  if (typeof window.ethereum !== "undefined") {
+    startLoading();
+
+    ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((accounts) => {
+        const account = accounts[0];
+
+        walletID.innerHTML = `Wallet connected: <span>${account}</span>`;
+
+        stopLoading();
+      })
+      .catch((error) => {
+        console.log(error, error.code);
+        stopLoading();
+      });
+  } else {
+    window.open("https://metamask.io/download/", "_blank");
+    installAlert.classList.add("show");
+  }
+});
+
+reloadButton.addEventListener("click", () => {
+  window.location.reload();
+});
